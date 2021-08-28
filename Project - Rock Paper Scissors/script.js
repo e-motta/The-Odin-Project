88f180174bfe018642c9d602aeddadf9a4f50c5e
Eduardo Motta de Moraes <edumoraes@live.com>
2021-08-28 19:42:18 -0300

    update script.js, refactor comments

--- Project - Rock Paper Scissors/script.js
+++ Project - Rock Paper Scissors/script.js
@@ -1,16 +1,10 @@
-// Function computerPlay: randomly return Rock, Paper, or Scissors
-// 1. Generate random number between 1 and 3
-// 2. Conditions: 1 = Rock; 2 = Paper; 3 = Scissors
-// 3. Return the corresopnding string
-function computerPlay() {
+function randomComputerPlay() {
     let possiblePlays = [`rock`, `paper`, `scissors`]
     let randomNumber = Math.floor(Math.random() * 3);
     return possiblePlays[randomNumber];
 }
 
 
-// Function userPlay: return user play
-// Make sure the play is valid; if invalid, try again
 function userPlay() {
     let input = prompt("Choose Rock, Paper or Scissors").toLowerCase();
     if (input !== `rock` && input !== `paper` && input !== `scissors`) {
@@ -22,16 +16,8 @@ function userPlay() {
 }
 
 
-// Function playRound: return string declaring winner/loser
-// Two parameters: playerSelection, computerSelection
-// Case insensitive
-// 1. Conditions:
-//    if player == Rock
-//      if computer == Rock: tie
-//      if computer == Paper: loss
-//      if computer == Scissors: win
-//      ...
-// 2. Return string (e.g. "You Lose! Paper beats Rock"), string (win, loss, tie)
+// Return array with two strings: [string (e.g. "You Lose! Paper beats Rock"), string ("win", "loss", or "tie")]
+// Second string will be used to tally up the score
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === `rock`) {
         if (computerSelection === `rock`) {
@@ -61,20 +47,13 @@ function playRound(playerSelection, computerSelection) {
 }
 
 
-// Function game: play a five-round game, keep score, report winner/loser
-// 1. Instantiate variables roundWins = 0, roundLosses = 0, roundTies = 0
-// 2. Loop five times:
-//    Get input from user (Rock, Paper, Scissors), make it lowercase, assign it to variable userPlay
-//    Call function computerPlay, assign it to variable compPlay
-//    Call function playRound, print first value, assign second value to variable win
-//    Log roundWins, roundLosses and roundTies (if result === win, roundWins++; else if result === loss roundLosses++ ...)
-// 3. Print winner and players wins, losses, ties
+// Play a five-round game, keep score, report winner/loser
 function game() {
     let roundWins = 0;
     let roundLosses = 0;
     let roundTies = 0;
     for (i = 0; i < 5; i++) {
-        let [message, result] = playRound(userPlay(), computerPlay());
+        let [message, result] = playRound(userPlay(), randomComputerPlay());
         if (result === "win") {
             roundWins++;
         } else if (result === "loss") {
