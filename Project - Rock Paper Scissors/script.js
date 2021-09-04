c86d6b06a8b238bdd61eb6fe94a0db6b83536460
Eduardo Motta de Moraes <edumoraes@live.com>
2021-09-04 08:21:24 -0300

    refactor function playRound, create function gameOver

--- Project - Rock Paper Scissors/script.js
+++ Project - Rock Paper Scissors/script.js
@@ -4,102 +4,80 @@ function randomComputerPlay() { // OK
     return possiblePlays[randomNumber];
 }
 
+function gameOver(playerScore, computerScore, roundPlays, roundResult) {
+    if (playerScore.innerText == 5 || computerScore.innerText == 5) {
+        // Announce game won or lost, ask if new game
+        console.log(`Game over`)
+        // Clears score
+        playerScore.innerText = 0;
+        computerScore.innerText = 0;
+        roundPlays.innerHTML = ``;
+        roundResult.innerHTML = ``;
+    }
+}
 
-// NOT NECESSARY FOR UI, DELETE LATER
 
-// function userPlay() {
-//     let input = prompt(`Choose Rock, Paper or Scissors`).toLowerCase();
-//     if (input !== `rock` && input !== `paper` && input !== `scissors`) {
-//         alert(`Invalid entry, try again`);
-//         return userPlay();
-//     } else {
-//         return input;
-//     }
-// }
-
-
-// Return array: [string (e.g. "You Lose! Paper beats Rock"), string ("win", "loss", or "tie")]
-// Second string will be used to tally up the score when playing the game
 function playRound(playerSelection, computerSelection) { // OK
-    const runningScore = document.getElementById("running score");
+    const roundPlays = document.getElementById(`round plays`);
+    const roundResult = document.getElementById(`round result`);
+
+    const playerScore = document.getElementById(`player score`);
+    const computerScore = document.getElementById(`computer score`);
+
+    roundPlays.innerHTML = `You chose ${playerSelection}. The opponent chose ${computerSelection}.`
+    
     if (playerSelection === `rock`) {
         if (computerSelection === `rock`) {
-            runningScore.innerHTML = `It's a tie! Both players chose rock`;
-            return [`It's a tie! Both players chose rock`, `tie`];
+            roundResult.innerHTML = `It's a tie! Both players chose rock.`;
+            gameOver(playerScore, computerScore, roundPlays, roundResult);
         } else if (computerSelection === `paper`) {
-            runningScore.innerHTML = `You lose! Paper beats rock`
-            return [`You lose! Paper beats rock`, `loss`];
+            roundResult.innerHTML = `You lose! Paper beats rock!`;
+            computerScore.innerText = +computerScore.innerText + 1;
+            gameOver(playerScore, computerScore, roundPlays, roundResult);
         } else {
-            runningScore.innerHTML = `You win! Rock beats scissors`
-            return [`You win! Rock beats scissors`, `win`];
+            roundResult.innerHTML = `You win! Rock beats scissors!`;
+            playerScore.innerText = +playerScore.innerText + 1;
+            gameOver(playerScore, computerScore, roundPlays, roundResult);
         }
     } else if (playerSelection === `paper`) {
         if (computerSelection === `rock`) {
-            runningScore.innerHTML = `You win! Paper beats rock`
-            return [`You win! Paper beats rock`, `win`];
+            roundResult.innerHTML = `You win! Paper beats rock!`;
+            playerScore.innerText = +playerScore.innerText + 1;
+            gameOver(playerScore, computerScore, roundPlays, roundResult);
         } else if (computerSelection === `paper`) {
-            runningScore.innerHTML = `It's a tie! Both player chose paper`
-            return [`It's a tie! Both player chose paper`, `tie`];
+            roundResult.innerHTML = `It's a tie! Both player chose paper.`;
         } else {
-            runningScore.innerHTML = `You lose! Scissors beat rock!`
-            return [`You lose! Scissors beat rock!`, `loss`];
+            roundResult.innerHTML = `You lose! Scissors beat rock!`;
+            computerScore.innerText = +computerScore.innerText + 1;
+            gameOver(playerScore, computerScore, roundPlays, roundResult);
         }
     } else {
         if (computerSelection === `rock`) {
-            runningScore.innerHTML = `You lose! Rock beats scissors`
-            return [`You lose! Rock beats scissors`, `loss`];
+            roundResult.innerHTML = `You lose! Rock beats scissors!`;
+            computerScore.innerText = +computerScore.innerText + 1;
+            gameOver(playerScore, computerScore, roundPlays, roundResult);
         } else if (computerSelection === `paper`) {
-            runningScore.innerHTML = `You win! Scissors beat paper`
-            return [`You win! Scissors beat paper`, `win`];
+            roundResult.innerHTML = `You win! Scissors beat paper!`;
+            playerScore.innerText = +playerScore.innerText + 1;
+            gameOver(playerScore, computerScore, roundPlays, roundResult);
         } else {
-            runningScore.innerHTML = `It's a tie! Both player chose scissors`
-            return [`It's a tie! Both player chose scissors`, `tie`];
+            roundResult.innerHTML = `It's a tie! Both player chose scissors.`;
         }
     }
 }
 
 
-// Play a five-round game, keep score, report winner/loser
-// function game() {
-//     let roundWins = 0;
-//     let roundLosses = 0;
-//     let roundTies = 0;
-//     for (i = 0; i < 5; i++) {
-//         let [message, result] = playRound(userPlay(), randomComputerPlay());
-//         console.log(message);
-//         if (result === `win`) {
-//             roundWins++;
-//         } else if (result === `loss`) {
-//             roundLosses++;
-//         } else {
-//             roundTies++;
-//         }
-//     }
-//     let score = `Wins: ${roundWins}, Losses: ${roundLosses}, Ties: ${roundTies}`
-//     if (roundWins > roundLosses) {
-//         console.log(`Conratulations, you win the game! ${score}`);
-//     } else if (roundWins < roundLosses) {
-//         console.log(`Unfortunately you lost the game. ${score}`)
-//     } else {
-//         console.log(`The game is a tie! ${score}`)
-//     }
-// }
-
-
-// function  {
-
-// }
-
-function click (e) {
-    playRound(e.path[0].id, randomComputerPlay());
-    const btn = e.path[0];
-    btn.classList.toggle('clicking');
+function click() {
+    playRound(this.id, randomComputerPlay());
+    this.classList.toggle('clicking');
 }
+
 
 function removeTransition(e) {
     if(e.propertyName !== 'transform') return;
     this.classList.remove('clicking');
 }
+
 
 const btns = document.querySelectorAll('.btn');
 btns.forEach(btn => btn.addEventListener('click', click))
