65a46ca364042a0562422ec6ae5a94e54216ef8a
Eduardo Motta de Moraes <edumoraes@live.com>
2021-09-06 07:41:54 -0300

    add class to each sqaure in js

--- Project - Etch-a-skecth/script.js
+++ Project - Etch-a-skecth/script.js
@@ -1,3 +1,4 @@
+// Create squares in fixed grid
 const container = document.querySelector('.grid-container');
 let numberOfLines = 16
 let numberOfSquares = numberOfLines ** 2;
@@ -6,3 +7,8 @@ for(let i = 0; i < numberOfSquares; i++) {
     const newDiv = document.createElement('div');
     container.append(newDiv);
 }
+
+// Add class to squares
+const squares = container.querySelectorAll('div');
+squares.forEach((e) => e.classList.add('squares'));
+
