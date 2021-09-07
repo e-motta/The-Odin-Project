a16a628e0b688bd1853485705b0ed97d0fa4d900
Eduardo Motta de Moraes <edumoraes@live.com>
2021-09-06 22:46:17 -0300

    add function to allow variable number of squares in the same container, fix typos, shorten documentation

--- Project - Etch-a-skecth/script.js
+++ Project - Etch-a-skecth/script.js
@@ -1,41 +1,38 @@
-// Create squares in fixed grid. Grid size is defined in CSS.
-const container = document.querySelector('.grid-container');
-let numberOfLines = 16
-let numberOfSquares = numberOfLines ** 2;
-
-for(let i = 0; i < numberOfSquares; i++) {
-    const square = document.createElement('div');
-    container.append(square);
+function createGrid(container, numberOfLines) { // FIXME: change CSS grid rows and columns
+    let numberOfSquares = numberOfLines ** 2;
+    for(let i = 0; i < numberOfSquares; i++) {
+        const square = document.createElement('div');
+        container.append(square);
+    }
+    let squares = container.querySelectorAll('div');
+    // Default color for grid.
+    squares.forEach((e) => e.classList.add('squares-default'));
+    // Change color when hovering mouse over grid.
+    squares.forEach((e) => e.addEventListener('mouseover', mouseHover));
 }
 
-const squares = container.querySelectorAll('div');
-
-// Default color for grid.
-squares.forEach((e) => e.classList.add('squares-default'));
-
-// Color when hovering mouse over grid.
 function mouseHover(e) {
     this.classList.add('squares-hover');
 }
-squares.forEach((e) => e.addEventListener('mouseover', mouseHover));
 
-// Clear grid when button is pressed.
 function clearGrid() {
-    squares.forEach((e) => e.classList.remove('squares-hover'));
+    while (container.firstChild) {
+        container.removeChild(container.lastChild);
+    }
+    const newNumberOfLines = prompt('Please, enter the number of line for the grid (0-100)'); // FIXME: limit 1-100; null; undefined
+    createGrid(container, newNumberOfLines);
+    this.classList.toggle('clicking');
 }
-function click(){
-    console.log(this)
-    this.classList.add('clicking');
-}
+
 function removeTransition(e) {
     if(e.propertyName !== 'transform') return;
     this.classList.remove('clicking');
 }
+
+const container = document.querySelector('.grid-container');
+
+createGrid(container, 16)
+
 const clearButton = document.querySelector('.clear-button');
-clearButton.addEventListener('click', () => clearGrid());
-clearButton.addEventListener('click', click);
+clearButton.addEventListener('click', clearGrid);
 clearButton.addEventListener('transitionend', removeTransition);
-
-
-// Ask user for number of squares when button is pressed.
-// Must update HTML and CSS.
