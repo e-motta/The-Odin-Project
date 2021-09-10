cc4f1f5d9daec29774ed2e76c3d725e2755b96cb
Eduardo Motta de Moraes <edumoraes@live.com>
2021-09-10 17:39:05 -0300

    create js, css, html; add operating functions

---
+++ Project - Calculator/script.js
@@ -0,0 +1,11 @@
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+const squared = (a) => a ** 2;
+const power = (a, b) => a ** b;
+const sqrt = (a) => a ** 1/2;
+
+function operate (operator, a, b) {
+    //
+}
