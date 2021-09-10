4fc59c085158615b54104b1a14392254e1fdb01e
Eduardo Motta de Moraes <edumoraes@live.com>
2021-09-10 19:29:26 -0300

    update operate function, partially add buttons html and css

--- Project - Calculator/script.js
+++ Project - Calculator/script.js
@@ -2,10 +2,22 @@ const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
-const squared = (a) => a ** 2;
-const power = (a, b) => a ** b;
-const sqrt = (a) => a ** 1/2;
 
 function operate (operator, a, b) {
-    //
+    if(operator === `+`) return add(a, b);
+    else if(operator === `-`) return subtract(a, b);
+    else if(operator === `*`) return multiply(a, b);
+    else if(operator === `/`) return divide(a, b);
 }
+
+function createButtons (buttonsContainer) {
+    for(let i = 0; i < 16; i++) {
+        const button = document.createElement('button');
+        buttonsContainer.append(button);
+    }
+    let buttons = buttonsContainer.querySelectorAll('button');
+    buttons.forEach((e) => e.classList.add('buttons'));
+}
+
+const buttonsContainer = document.querySelector('.buttons-container');
+createButtons(buttonsContainer)
